392c8cfe065d0050dbf2b2746f05e139bffdd010
Vivek Alhat <vivekalhat14@outlook.com>
2024-01-31 22:41:50 +0530

    enable vercel analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import { Outfit } from "next/font/google";
 import "./globals.css";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Toaster />
+	<Analytics />
       </body>
     </html>
   );
