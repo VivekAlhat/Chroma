eb5c1a45ba7cfc2b14803c823dcfe92c8aa2c751
Vivek Alhat <vivekalhat14@outlook.com>
2024-01-31 23:24:45 +0530

    add logger and rate limiter

--- server/chroma.go
+++ server/chroma.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"sort"
+	"time"
 
-	"github.com/VivekAlhat/Chroma/server/helpers"
-	"github.com/VivekAlhat/Chroma/server/kmeans"
+	"github.com/VivekAlhat/Chroma/server/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
-
-const K = 5
-const MAX_ITERATIONS = 500
-
-type Color struct {
-	RGB        kmeans.Point `json:"rgb"`
-	Hex        string       `json:"hex"`
-	Percentage int          `json:"percentage"`
-}
-
-type Palette struct {
-	Name    string  `json:"name"`
-	Palette []Color `json:"palette"`
-}
 
 func main() {
 	app := fiber.New(fiber.Config{
@@ -32,60 +17,19 @@ func main() {
 	})
 
 	app.Use(cors.New())
+	app.Use(logger.New())
+	app.Use(limiter.New(limiter.Config{
+		Max:               5,
+		Expiration:        60 * time.Minute,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		c.SendString("Hello Chroma")
 		return nil
 	})
 
-	app.Post("/upload", func(c *fiber.Ctx) error {
-		form, err := c.MultipartForm()
-		if err != nil {
-			return err
-		}
-
-		var palettes []Palette
-		for _, headers := range form.File {
-			for _, file := range headers {
-				colors := make([]Color, 0)
-
-				destination := fmt.Sprintf("./tmp/%s", file.Filename)
-				if err := c.SaveFile(file, destination); err != nil {
-					return err
-				}
-
-				img, err := helpers.LoadImage(destination)
-				if err != nil {
-					return err
-				}
-
-				resized := helpers.ResizeImage(img, 100)
-				points := helpers.ExtractColors(resized)
-
-				clusters := kmeans.KMeans(points, K, MAX_ITERATIONS)
-
-				for i := range clusters {
-					percentage := helpers.CalculatePercentage(len(clusters[i].Points), len(points))
-					hex := helpers.ConvertToHEX(clusters[i].Center)
-					color := Color{RGB: clusters[i].Center, Hex: hex, Percentage: int(percentage)}
-					colors = append(colors, color)
-				}
-
-				sort.Slice(colors, func(i, j int) bool {
-					return colors[i].Percentage > colors[j].Percentage
-				})
-
-				palette := Palette{Name: file.Filename, Palette: colors}
-				palettes = append(palettes, palette)
-
-				deletion := os.Remove(destination)
-				if deletion != nil {
-					return deletion
-				}
-			}
-		}
-		return c.JSON(palettes)
-	})
+	app.Post("/upload", handlers.GeneratePalette)
 
 	log.Fatal(app.Listen(":8000"))
 }
---
+++ server/handlers/handlers.go
@@ -0,0 +1,74 @@
+package handlers
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/VivekAlhat/Chroma/server/helpers"
+	"github.com/VivekAlhat/Chroma/server/kmeans"
+	"github.com/gofiber/fiber/v2"
+)
+
+const K = 5
+const MAX_ITERATIONS = 500
+
+type Color struct {
+	RGB        kmeans.Point `json:"rgb"`
+	Hex        string       `json:"hex"`
+	Percentage int          `json:"percentage"`
+}
+
+type Palette struct {
+	Name    string  `json:"name"`
+	Palette []Color `json:"palette"`
+}
+
+func GeneratePalette(c *fiber.Ctx) error {
+	form, err := c.MultipartForm()
+	if err != nil {
+		return err
+	}
+
+	var palettes []Palette
+	for _, headers := range form.File {
+		for _, file := range headers {
+			colors := make([]Color, 0)
+
+			destination := fmt.Sprintf("./tmp/%s", file.Filename)
+			if err := c.SaveFile(file, destination); err != nil {
+				return err
+			}
+
+			img, err := helpers.LoadImage(destination)
+			if err != nil {
+				return err
+			}
+
+			resized := helpers.ResizeImage(img, 100)
+			points := helpers.ExtractColors(resized)
+
+			clusters := kmeans.KMeans(points, K, MAX_ITERATIONS)
+
+			for i := range clusters {
+				percentage := helpers.CalculatePercentage(len(clusters[i].Points), len(points))
+				hex := helpers.ConvertToHEX(clusters[i].Center)
+				color := Color{RGB: clusters[i].Center, Hex: hex, Percentage: int(percentage)}
+				colors = append(colors, color)
+			}
+
+			sort.Slice(colors, func(i, j int) bool {
+				return colors[i].Percentage > colors[j].Percentage
+			})
+
+			palette := Palette{Name: file.Filename, Palette: colors}
+			palettes = append(palettes, palette)
+
+			deletion := os.Remove(destination)
+			if deletion != nil {
+				return deletion
+			}
+		}
+	}
+	return c.JSON(palettes)
+}
